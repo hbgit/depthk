24c1383d54eb79948550c4f8a6c30958c299006d
Herbert <herbert@localhost.localdomain>
2014-12-22 04:36:27 -0400

    Added PyCparser to the project
    Signed-off-by: Herbert <herbert@localhost.localdomain>

---
+++ depthk.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: latin1 -*-
+# -------------------------------------------------
+# DepthK v1.0
+# by Herbert Rocha
+#
+# e-mail: herberthb12@gmail.com
+# -------------------------------------------------
+
+from __future__ import print_function
+
+
+# TODO:
+# (1) Identify each variable and it type [DONE] << Test with output PIPS
+# (2) Read the code.pips identify #init, save the VAR#init and the function where it was find
+# (3) write new code where for each function identified in (2) we should add the new vars to VAR#init
+# (4) Read the code.pips and translate the annotations to __ESBMC. WARNNING: replace VAR#init to VAR_init
+
+
+
